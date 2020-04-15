8c5a0087fe56d670eb3f0aebd7bd837be51357c2
Karim <karimelzanaty98@gmail.com>
2020-04-14 21:48:27 -0400

    Setup in progress!

---
+++ PolyPong/Assets/Code/AndrickPlugin.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class AndrickPlugin
+{
+    [DllImport("AndrickPlugin")]
+    public static extern void CreateDefaultQuaternion(bool identity, float[] quaternion);
+
+    [DllImport("AndrickPlugin")]
+    public static extern void CreateQuaternion(float[] vec3, float angle, bool isDegrees, float[] quaternion);
+
+    [DllImport("AndrickPlugin")]
+    public static extern void Normalize(float[] quaternion, float[] r);
+
+    [DllImport("AndrickPlugin")]
+    public static extern void Inverted(float[] quaternion, float[] r);
+
+    [DllImport("AndrickPlugin")]
+    public static extern void Multiply(float[] q1, float[] q2, float[] r);
+
+    [DllImport("AndrickPlugin")]
+    public static extern void MultiplyWithVec(float[] q1, float[] vec3, float[] r);
+
+    [DllImport("AndrickPlugin")]
+    public static extern void Scale(float[] q1, float scalar, float[] r);
+
+    [DllImport("AndrickPlugin")]
+    public static extern void Add(float[] q1, float[] q2, float[] r);
+
+    [DllImport("AndrickPlugin")]
+    public static extern void Rotate(float[] q1, float[] vec3, float[] r);
+
+    [DllImport("AndrickPlugin")]
+    public static extern float GetMagnitude(float[] quaternion);
+
+    [DllImport("AndrickPlugin")]
+    public static extern float GetMagnitudeSquared(float[] quaternion);
+}
---
+++ PolyPong/Assets/Code/TestingGarbage/TestDLL.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDLL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float[] quatBaby = new float[4];
+        AndrickPlugin.CreateDefaultQuaternion(true, quatBaby);
+        Debug.Log(quatBaby);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
