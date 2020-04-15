ded11c5bb76a942fa1d0149cc9229b726f75becd
Andrew Rimpici <AndrewR608@gmail.com>
2020-04-14 23:18:31 -0400

    Adding in starting content

---
+++ PolyPong/Assets/Code/Scene/ProgressBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ProgressBar : MonoBehaviour
+{
+    public static readonly float MAX_LOAD_VALUE = 0.9f;
+    private SceneInfo mPrevSceneInfo;
+    
+    public virtual IEnumerator UpdateProgressBar(AsyncOperation loadOperation)
+    {
+        while (!loadOperation.isDone)
+        {
+            float progress = loadOperation.progress / MAX_LOAD_VALUE;
+            Debug.Log(string.Format("Progress: {0}", progress));
+            yield return null;
+        }
+    }
+}
---
+++ PolyPong/Assets/Code/Scene/SceneBase.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneBase : MonoBehaviour
+{
+    private static GameObject instance;
+
+    protected void Awake()
+    {
+        if (!instance)
+        {
+            instance = Instantiate(Resources.Load<GameObject>("Prefab/Persistent/POLY_PONG_CORE"));
+            instance.name = "POLY_PONG_CORE";
+            
+            SceneTracker sceneTracker = instance.GetComponent<SceneTracker>();
+            sceneTracker.LoadSceneAsync(SceneInfoList.CAMERA_MANAGER);
+            
+            //instance.GetComponent<SceneTracker>().LoadSceneAsync(SceneInfoList.SCENE_SWITCHER);
+        }
+    }
+}
