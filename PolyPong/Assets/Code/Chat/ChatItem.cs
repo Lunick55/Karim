c243563ff45af1df55ae56316d32f5f7db72f58d
Andrew Rimpici <AndrewR608@gmail.com>
2020-04-26 05:18:04 -0400

    Added in ability to add chats to a chat log

---
+++ PolyPong/Assets/Code/Chat/ChatItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChatItem : MonoBehaviour
+{
+    public TextMeshProUGUI ChatItemText;
+
+    public void PopulateMessage(string Username, string Message)
+    {
+        ChatItemText.text = string.Format("{0}: {1}", Username, Message);
+    }
+}
---
+++ PolyPong/Assets/Code/Scene/Lobby.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Lobby : SceneBase<Lobby>
+{
+    public TextMeshProUGUI ChatMessageBox;
+    private ChatLog LobbyChatLog;
+    private bool IsGameStarting;
+
+    public void Start()
+    {
+        LobbyChatLog = GetComponent<ChatLog>();
+        IsGameStarting = false;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+            OnSendClicked();
+    }
+
+    public void OnSendClicked()
+    {
+        LobbyChatLog.AddChatItem("NOOB", ChatMessageBox.text);
+    }
+
+    public void OnStartGameClicked()
+    {
+        if (!IsGameStarting)
+        {
+            StartCoroutine(StartGame());
+        }
+    }
+
+    private IEnumerator StartGame()
+    {
+        if (IsGameStarting)
+        {
+            Debug.LogWarning("Already counting down!");
+            yield return null;
+        }
+        else
+        {
+            IsGameStarting = true;
+
+            int MaxCount = 3;
+            int StartCounter = 0;
+
+            while (StartCounter <= MaxCount)
+            {
+                LobbyChatLog.AddChatItem("Server", string.Format("Starting game in {0}!", MaxCount - StartCounter));
+                yield return new WaitForSeconds(1.0f);
+            }
+
+            IsGameStarting = false;
+        }
+    }
+}
